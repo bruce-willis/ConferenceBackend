b45592e54eb134550882ff925f448ad6d7a7aab1
Yury <Yury@BRUCE_PC>
2016-09-03 14:12:03 +0300

    Add project files.

---
+++ sandboxappService/App_Start/Startup.MobileApp.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Web.Http;
+using Microsoft.Azure.Mobile.Server;
+using Microsoft.Azure.Mobile.Server.Authentication;
+using Microsoft.Azure.Mobile.Server.Config;
+using sandboxappService.DataObjects;
+using sandboxappService.Models;
+using Owin;
+
+namespace sandboxappService
+{
+  public partial class Startup
+  {
+    public static void ConfigureMobileApp(IAppBuilder app)
+    {
+      HttpConfiguration config = new HttpConfiguration();
+
+      //For more information on Web API tracing, see http://go.microsoft.com/fwlink/?LinkId=620686 
+      config.EnableSystemDiagnosticsTracing();
+
+      new MobileAppConfiguration()
+          .UseDefaultConfiguration()
+          .ApplyTo(config);
+
+      // Use Entity Framework Code First to create database tables based on your DbContext
+      Database.SetInitializer(new sandboxappInitializer());
+
+      // To prevent Entity Framework from modifying your database schema, use a null database initializer
+      // Database.SetInitializer<sandboxappContext>(null);
+
+      MobileAppSettingsDictionary settings = config.GetMobileAppSettingsProvider().GetMobileAppSettings();
+
+      if (string.IsNullOrEmpty(settings.HostName))
+      {
+        // This middleware is intended to be used locally for debugging. By default, HostName will
+        // only have a value when running in an App Service application.
+        app.UseAppServiceAuthentication(new AppServiceAuthenticationOptions
+        {
+          SigningKey = ConfigurationManager.AppSettings["SigningKey"],
+          ValidAudiences = new[] { ConfigurationManager.AppSettings["ValidAudience"] },
+          ValidIssuers = new[] { ConfigurationManager.AppSettings["ValidIssuer"] },
+          TokenHandler = config.GetAppServiceTokenHandler()
+        });
+      }
+      app.UseWebApi(config);
+    }
+  }
+
+  public class sandboxappInitializer : CreateDatabaseIfNotExists<sandboxappContext>
+  {
+    protected override void Seed(sandboxappContext context)
+    {
+      List<Conference> conferences = new List<Conference>
+      {
+        new Conference {Action = "all is good", Number = 42},
+        new Conference {Action = "Serega pidr", Number = 7}
+      };
+      foreach (Conference conference in conferences)
+      {
+        context.Set<Conference>().Add(conference);
+      }
+      base.Seed(context);
+    }
+  }
+}
+
---
+++ sandboxappService/DataObjects/Conference.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Azure.Mobile.Server;
+
+namespace sandboxappService.DataObjects
+{
+  public class Conference : EntityData
+  {
+    public int Number { get; set; }
+
+    public string Action { get; set; }
+
+  }
+}
